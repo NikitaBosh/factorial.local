fd5cd8cb762df26a0cef2441739a2b5344f9fc67
NikitaBosh <nrandryuha@mail.ru>
2021-03-17 10:27:06 +0300

    1.4 Создание метода для загрузки формы

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class CalculateController extends Controller
 {
-    //
+    /**
+     * Загрузка формы
+     *
+     *
+     * @return     View представление
+     */
+    public function index()
+    {
+        return view('index')
+    }
 }
