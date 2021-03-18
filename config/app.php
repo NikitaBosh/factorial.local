6f185b1a2877ebda17b08173ed0b4785bafcd6f2
NikitaBosh <nrandryuha@mail.ru>
2021-03-18 10:11:14 +0300

    2.5. Подключение сервис провайдера

--- config/app.php
+++ config/app.php
@@ -175,6 +175,13 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+                App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Our service providers
+         */
+        App\Providers\CalculateServiceProvider::class,
     ],
 
     /*
